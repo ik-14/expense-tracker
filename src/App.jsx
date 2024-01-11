df7d4683967ea36f4549db772b8703ad9b3524fd
Ismail Khalil <ismailkhalil@hollandandbarrett.com>
2024-01-11 14:33:40 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import "./App.css";
+import React, { useState } from "react";
+import {
+  Center,
+  Heading,
+  FormControl,
+  FormLabel,
+  NumberInput,
+  NumberInputField,
+  Button,
+  Flex,
+  Box,
+  Stat,
+  StatLabel,
+  StatNumber,
+} from "@chakra-ui/react";
+
+function App() {
+  const [budget, setBudget] = useState(0);
+  const [budgetSubmitted, setBudgetSubmitted] = useState(false);
+
+  console.log(budgetSubmitted);
+
+  const handleSubmit = () => {
+    setBudgetSubmitted(true);
+    console.log("Submitted Budget:", budget);
+  };
+
+  return (
+    <>
+      <Center w="100%">
+        <Heading>Expense tracker</Heading>
+      </Center>
+
+      <Flex w="100%">
+        {/* Left side: Budget form */}
+        <Box flex="1" bg="#BEBEB8">
+          <FormControl>
+            <Center w="100">
+              <FormLabel htmlFor="myInput" fontSize="xl">
+                Budget 💰
+              </FormLabel>
+            </Center>
+            <NumberInput
+              onChange={(valueNumber) => {
+                setBudget(valueNumber);
+                setBudgetSubmitted(false);
+              }}
+            >
+              <NumberInputField placeholder="Enter a budget" />
+            </NumberInput>
+            <Center>
+              <Button mt={4} colorScheme="teal" onClick={handleSubmit}>
+                Submit
+              </Button>
+            </Center>
+            {budgetSubmitted && budget > 0 && (
+              <Stat>
+                <StatLabel>Budget</StatLabel>
+                <StatNumber>{budget}</StatNumber>
+              </Stat>
+            )}
+          </FormControl>
+        </Box>
+
+        {/* Right side: "hi" div */}
+        <Box flex="2" bg="#E3E3DA">
+          <div>hi</div>
+        </Box>
+      </Flex>
+    </>
+  );
+}
+
+export default App;
